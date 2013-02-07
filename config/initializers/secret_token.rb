47698599153acad15a02067b536ee7136ce540ea
Vincent Chang <vhchang@gmail.com>
2013-02-07 09:21:13 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wheelio::Application.config.secret_token = '4b4fc1a5cf1a9d8479d0cbfb711dd8102418e7fe3d954da52df22d42ffa9cd13328c7fd05d6df962e179460cbf5c29771f321f2ff488d93304b9f52738a311cf'
